0d3539c121ece877254beff2b87130e71a75ee98
ibsen4 <115497034+ibsen4@users.noreply.github.com>
2022-12-08 21:43:43 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,25 @@
+bool letteraono(char valore){
+    if (valore>='A'&& valore<='Z' || valore>='a' && valore<='z'){
+        return true;
+    }
+    else
+        return false;
+}
+
+char conversionelettere(char valore){
+     if(letteraono(valore)==true){
+        char esito;
+        if( valore>='A' && valore<='Z'){
+         esito=valore+32;
+        }
+        else if(valore>='a' && valore<='z'){
+         esito=valore-32;
+        }
+        return esito;
+        }
+     else if(letteraono(valore)==false){
+         bool esito=false;
+         return esito;
+     }
+}
 
