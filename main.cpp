a301b7100850bad078ffc39af36ed383d2e643b1
ibsen4 <115497034+ibsen4@users.noreply.github.com>
2022-12-08 21:44:37 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "PrimaLibreria.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    char valore;
+    cin>>valore;
+    if(letteraono(valore)==true) {
+        cout << conversionelettere(valore);
+    }
+    else if (letteraono(valore)==false){
+        cout<<"errore";
+    }
+    return 0;
 }
